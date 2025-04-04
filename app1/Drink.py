aa49fae66491b1db180b197549c26637d0ab90e1
Mehmet Canevi <m.canevi3@gmail.com>
2025-04-05 01:55:19 +0300

    drink

--- app1/Drink.py
+++ app1/Drink.py
@@ -8,13 +8,24 @@ class Drink:
     calories:int
     portions:list
 
-    def __init__(self):
-        self.amount=200
-        self.brand=""
-        self.name="Water"
-        self.caffein=0
-        self.calories=0
-        self.portions=[1,10,100,200,330,500,1000,1500,2500]
+    def __init__(self,*arg):
+        if len(arg)==0:
+            self.amount=200
+            self.brand=""
+            self.name="Water"
+            self.caffein=0
+            self.calories=0
+            self.portions=[1,10,100,200,330,500,1000,1500,2500]
+        elif len(arg)==1 and type(arg[0])==dict:
+            val=arg[0]
+            self.amount=val['amount']
+            self.brand=val['brand']
+            self.name=val['name']
+            self.caffein=val['caffein']
+            self.calories=val['calories']
+            self.portions=val['portions']
+        else:
+            assert False , "Zero or one argument expected!"
     
     def dict(self):
         return dict(amount=self.amount,brand=self.brand,name=self.name,caffein=self.caffein,calories=self.calories,portions=self.portions)
@@ -23,35 +34,49 @@ class Drink:
         print(self.dict())
 
 class DrinkDB:
-    drink=None
     table_name="drink"
     def __init__(self,file_name:str):
         self.db=Database(file_name)
-        self.drink=Drink()
-    def set_drink(self,drink:Drink):
-        self.drink=drink
-    def set_db(self,db:Database):
-        self.db=db
+
     def create_table(self):
         self.db.create_table(self.table_name,names=["amount","brand","name","caffein","calories","portions"],types=[int,str,str,int,int,str])
+
     def load(self):
-        pass
-    def save(self):
+        res=self.db.select_all(self.table_name)
+        result=[]
+        for row in res:
+            portions_str=row[5]
+            portions_vec=[int(val) for val in str.split(portions_str,",")]
+
+            drink=Drink(dict(amount=int(row[0]),brand=row[1],name=row[2],caffein=int(row[3]),calories=int(row[4]),portions=portions_vec))
+            result.append(drink)
+        return result
+    
+    def update(self,drink:Drink):
+        # UPDATE employees SET lastname = 'Smith' WHERE employeeid = 3;
+        query=f"UPDATE {self.table_name} SET "
+        
+        res=self.db.select(self.table_name,["name","brand"],[drink.name,drink.brand])
+        print(res)                                                                                                                                                                                                                                                                                                                                                                             
+        return                                                                                                                       
+        for key,value in drink.dict().items():
+            print(key,value)
+
+    def save(self,drink:Drink):
         # drink to database
-        d=self.drink
         db=self.db
         if not db.has_table(self.table_name):
             self.create_table()
         # search for this one
-        res=db.select(self.table_name,["name","brand"],[d.name,d.brand])
+        res=db.select(self.table_name,["name","brand"],[drink.name,drink.brand])
         if len(res)==0:
             # insert this
-            d.print()
-            portions_str=[str(v) for v in d.portions]
-            db.insert(self.table_name,["amount","brand","name","caffein","calories","portions"],[d.amount,d.brand,d.name,d.caffein,d.calories,",".join(portions_str)])
+            drink.print()
+            portions_str=[str(v) for v in drink.portions]
+            db.insert(self.table_name,["amount","brand","name","caffein","calories","portions"],[drink.amount,drink.brand,drink.name,drink.caffein,drink.calories,",".join(portions_str)])
         elif len(res)==1:
             # need to update
-            d.print()
+            drink.print()
             pass
         else:
             # something went wrong
@@ -60,4 +85,10 @@ class DrinkDB:
         
 if __name__=="__main__":
     bridge=DrinkDB("drink1.sqlite")
-    bridge.save()
+    d=Drink({'amount': 200, 'brand': 'starbucks', 'name': 'mocha', 'caffein': 140, 'calories': 550, 'portions': [100, 200, 500, 750, 1000]})
+    bridge.update(d)
+    # bridge.save(d)
+
+    # res=bridge.load()
+    # for r in res:
+    #     r.print()
